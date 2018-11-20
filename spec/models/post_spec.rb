b5d68aa8e64f42fbceca28794d7bcfe8ababcc6e
Chris Franklin <cf73021@gmail.com>
2018-11-20 22:16:44 +0000

    automatically backed up by learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe Post do
-  post = Post.create!(title: "My title", description: "The post description")
-  expect(post).to be_valid
-end
+  describe Post do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
 end
